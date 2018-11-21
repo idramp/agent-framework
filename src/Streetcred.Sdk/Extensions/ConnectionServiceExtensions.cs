6b9318d7f7d7dfde0fe0235ac6e6dc5977878a34
Tomislav Markovski <tmarkovski@gmail.com>
2018-11-21 11:39:29 -0500

    Add connection service extensions with paging support

--- src/Streetcred.Sdk/Extensions/ConnectionServiceExtensions.cs
+++ src/Streetcred.Sdk/Extensions/ConnectionServiceExtensions.cs
@@ -26,6 +26,20 @@ namespace Streetcred.Sdk.Contracts
                 new SearchRecordQuery {{ TagConstants.State, ConnectionState.Negotiating.ToString("G")}}, count);
 
         /// <summary>
+        /// Retrieves a list of <see cref="ConnectionRecord" /> that are in <see cref="ConnectionState.Negotiating" /> state.
+        /// </summary>
+        /// <param name="connectionService">Connection service.</param>
+        /// <param name="wallet">Wallet.</param>
+        /// <param name="page">The page.</param>
+        /// <returns>
+        /// The negotiating connections async.
+        /// </returns>
+        public static Task<PagedCollection<ConnectionRecord>> ListNegotiatingConnectionsAsync(
+            this IConnectionService connectionService, Wallet wallet, Page page)
+            => connectionService.ListAsync(wallet,
+                new SearchRecordQuery { { TagConstants.State, ConnectionState.Negotiating.ToString("G") } }, page);
+
+        /// <summary>
         /// Retrieves a list of <see cref="ConnectionRecord"/> that are in <see cref="ConnectionState.Connected"/> state.
         /// </summary>
         /// <returns>The connected connections async.</returns>
@@ -38,6 +52,20 @@ namespace Streetcred.Sdk.Contracts
                 new SearchRecordQuery {{ TagConstants.State, ConnectionState.Connected.ToString("G")}}, count);
 
         /// <summary>
+        /// Retrieves a list of <see cref="ConnectionRecord" /> that are in <see cref="ConnectionState.Connected" /> state.
+        /// </summary>
+        /// <param name="connectionService">Connection service.</param>
+        /// <param name="wallet">Wallet.</param>
+        /// <param name="page">The page.</param>
+        /// <returns>
+        /// The connected connections async.
+        /// </returns>
+        public static Task<PagedCollection<ConnectionRecord>> ListConnectedConnectionsAsync(
+            this IConnectionService connectionService, Wallet wallet, Page page)
+            => connectionService.ListAsync(wallet,
+                new SearchRecordQuery { { TagConstants.State, ConnectionState.Connected.ToString("G") } }, page);
+
+        /// <summary>
         /// Retrieves a list of <see cref="ConnectionRecord"/> that are in <see cref="ConnectionState.Invited"/> state.
         /// </summary>
         /// <returns>The invited connections async.</returns>
@@ -48,5 +76,19 @@ namespace Streetcred.Sdk.Contracts
             this IConnectionService connectionService, Wallet wallet, int count = 100)
             => connectionService.ListAsync(wallet,
                 new SearchRecordQuery {{ TagConstants.State, ConnectionState.Invited.ToString("G")}}, count);
+
+        /// <summary>
+        /// Retrieves a list of <see cref="ConnectionRecord" /> that are in <see cref="ConnectionState.Invited" /> state.
+        /// </summary>
+        /// <param name="connectionService">Connection service.</param>
+        /// <param name="wallet">Wallet.</param>
+        /// <param name="page">The page.</param>
+        /// <returns>
+        /// The invited connections async.
+        /// </returns>
+        public static Task<PagedCollection<ConnectionRecord>> ListInvitedConnectionsAsync(
+            this IConnectionService connectionService, Wallet wallet, Page page)
+            => connectionService.ListAsync(wallet,
+                new SearchRecordQuery { { TagConstants.State, ConnectionState.Invited.ToString("G") } }, page);
     }
 }
